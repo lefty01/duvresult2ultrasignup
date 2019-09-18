efc12f5e16cd1a23cd7626836290ce1eecacc261
andreas <al@exitzero.de>
2019-09-18 15:25:24 +0200

    replace umlaute and try decode other diacritical marks from names. get event year from webpage table

--- duvresult2ultrasignup.py
+++ duvresult2ultrasignup.py
@@ -8,46 +8,87 @@ import time
 from bs4 import BeautifulSoup
 import csv
 import argparse
+from unidecode import unidecode
+import sys
+
+umlautDictionary = {
+    u'Ä': 'Ae',
+    u'Ö': 'Oe',
+    u'Ü': 'Ue',
+    u'ä': 'ae',
+    u'ö': 'oe',
+    u'ü': 'ue',
+    u'ß': 'ss'
+}
+
+def tr_umlaut(s):
+    umap = { ord(key):str(val) for key, val in umlautDictionary.items() }
+    return s.translate(umap)
+
 
 parser = argparse.ArgumentParser(description='convert duv stat results to csv list suitable to upload to ultrasignup')
-parser.add_argument('-e', '--event', required=True,
-                        help='DUV event ID')
-parser.add_argument('-y', '--year',  type=int, required=True,
-                        help='year of the event (to get runners age)')
+parser.add_argument('-e', '--event', required=True, help='DUV event ID')
+parser.add_argument('-y', '--year', type=int, help='year of the event (to get runners age)')
+parser.add_argument('-v', '--verbose', action='count', default=0, help='Verbose output (-vv increase verbosity')
+parser.add_argument('-d', '--debug',   action='store_true', help='Print debug output')
 args = parser.parse_args()
 
-event_year = args.year
 eventid = str(args.event)
-
 duv = 'http://statistik.d-u-v.org/getresultevent.php?event='
-result_file = 'results-event-' + eventid + '.csv'
 event_url = duv + eventid
 
-print("getting results for event #" + eventid + " from url: " + event_url)
+if args.verbose:
+    print("getting results for event #" + eventid + " from url: " + event_url)
+
 
 # FIXME handle error
 resp = 'n/a'
 resp = requests.get(event_url)
 soup = BeautifulSoup(resp.text, "html.parser")
 
+
+# todo: figure out event "type" i.e. distance race, timed event (eg. 24h) or backyard
+
+
+# note: that the duv site/table layout might change in the future
+# get event year from webpage, assume forth table contains race info
+# (event name, date, distance,...) try to get event year from this table
+table4 = soup.findAll('table')[3]
+date_row = table4.findAll('tr')[0]
+date1 = date_row.findAll('td')[0].text.strip()
+date2 = date_row.findAll('td')[1].text.strip()
+if date1 == "Date:":
+    try:
+        event_year = int(date2.split('.')[2])
+    except ValueError:
+        print("Warn: could not get event year from result page", file=sys.stderr)
+        if not args.year:
+            print("Try specifying the year via --year option", file=sys.stderr)
+            sys.exit(1)
+
+    if args.verbose:
+        print("Found event Year: " + str(event_year))
+
+if args.year:
+    if event_year:
+        print("Warn: overriding event year: " + str(event_year), file=sys.stderr)
+    event_year = args.year
+
+result_file = 'results-event-' + eventid + '-' + str(event_year) + '.csv'
+if args.verbose:
+    print("Writing results to file: " + result_file)
+
+# get event name and print if verbose
+event_row = table4.findAll('tr')[1]
+if event_row.findAll('td')[0].text.strip() == "Event:":
+    event_name = event_row.findAll('td')[1].text.strip()
+    if args.verbose:
+        print("Event: " + event_name)
+
+# get result table, actually get table body only by id
 results = soup.find("tbody", id="EvtRslt")
-#print("result list:\n" + str(results))
-
-
-# [0] place:          <td nowrap='nowrap' align='center'>1</td>
-# [1] time:           <td nowrap='nowrap' align='right'>12:24:50 h&nbsp;</td>
-# [2]!first,last: <td nowrap='nowrap' align='left'><a href='getresultperson.php?runner=123456'>&nbsp;xxxxxxxx, xxxxxx</a></td>
-#    <td nowrap='nowrap' align='left'> &nbsp;</td>
-# [4] state: ?        <td nowrap='nowrap' align='center'>GER&nbsp;</td>
-# [5] age:            <td nowrap='nowrap' align='center'>19xx</td>
-# [6] gender:         <td nowrap='nowrap' align='center'>M</td>
-#    <td nowrap='nowrap' align='center'>n</td>
-#    <td nowrap='nowrap' align='center'>Mxx&nbsp;</td>
-#    <td nowrap='nowrap' align='center'>1</td>
-#    <td nowrap='nowrap' align='right'>9.264</td>
-#    <td nowrap='nowrap' align='right'>10:38:29 h</td>
-
-
+if args.debug:
+    print("result list:\n" + str(results))
 
 
 output_rows = []
@@ -57,21 +98,19 @@ for results_row in results.findAll('tr'):
 
     place = cols[0].text
     time = cols[1].text.split()[0]
-    last, first = [s.strip() for s in cols[2].text.split(',')]
-    # age: get year of event then time diff current year - year of event
-    age = event_year - int(cols[5].text.strip())
+    name = cols[2].findAll('a')[0].text
+    last, first = [unidecode(tr_umlaut(s.strip())) for s in name.split(',')]
+    # I found runners without birthday ... ultra-runners are strange people ;)
+    try:
+        runner_yob = int(cols[5].text.strip())
+    except ValueError:
+        print("Warn: no year of birth for runner: " + name + ", skipping", file=sys.stderr)
+        continue
+
+    age = event_year - runner_yob
     gender = cols[6].text.strip()
     state  = cols[4].text.strip()
     
-    # print("place:  " + place)
-    # print("time:   " + time)
-    # print("first:  " + first)
-    # print("last:   " + last)
-    # print("age:    " + str(age))
-    # print("gender: " + gender)
-    # print("state:  " + state)
-    # print("")
-
     output_row.append(place)
     output_row.append(time)
     output_row.append(first)
@@ -81,12 +120,13 @@ for results_row in results.findAll('tr'):
     output_row.append(state)
 
     output_rows.append(output_row)
-    print("out row: " + str(output_row))
+    if args.verbose > 1:
+        print("Found: " + str(output_row))
 
 
-with open(result_file, 'w', encoding='utf-8') as f:
+with open(result_file, 'w') as f:
     writer = csv.writer(f)
     # csv minimum headers required: place, time|distance, first, last, age, gender
     # todo: distance for times event eg. 24h runs
-    writer.writerow(['place', 'time', 'first', 'last', 'age', 'gender'])
+    writer.writerow(['place', 'time', 'first', 'last', 'age', 'gender', 'state'])
     writer.writerows(output_rows)
